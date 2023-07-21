258ba1af057f22a7c121de68d556e91e2c0de5fc
ilhwan <poj0722@naver.com>
2023-07-21 18:58:21 +0900

    :tada: 프로젝트 생성

---
+++ src/main/kotlin/com/pppp0722/nulllovebe/NullloveBeApplication.kt
@@ -0,0 +1,11 @@
+package com.pppp0722.nulllovebe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NullloveBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<NullloveBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/pppp0722/nulllovebe/NullloveBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pppp0722.nulllovebe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NullloveBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
